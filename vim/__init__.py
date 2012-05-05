8e5373e3a8b3ada5d6f640781646da14c33089ac
Martin Carpenter <mcarpenter@free.fr>
2012-05-05 18:36:37 +0200

    Vim lens, first cut

--- vim/__init__.py
+++ vim/__init__.py
@@ -1,5 +1,7 @@
 import logging
 import optparse
+import re
+import subprocess
 
 import gettext
 from gettext import gettext as _
@@ -9,25 +11,105 @@ from singlet.lens import SingleScopeLens, IconViewCategory, ListViewCategory
 
 from vim import vimconfig
 
+from gi import _glib
+from gi.repository import Gio
+from os.path import expanduser, join
+from fnmatch import fnmatch
+
 class VimLens(SingleScopeLens):
 
     class Meta:
         name = 'vim'
         description = 'Vim Lens'
-        search_hint = 'Search Vim'
+        search_hint = 'Search Vim history'
         icon = 'vim.svg'
-        search_on_blank=True
+        search_on_blank = True
 
-    # TODO: Add your categories
-    example_category = ListViewCategory("Examples", 'help')
+    vim_icon = '/usr/share/app-install/icons/vim.png'
+    viminfo = join(expanduser('~'), '.viminfo')
+
+    # ListView view shows the full path
+    #vimfiles_category = IconViewCategory('Vim files', 'dialog-information-symbolic')
+    vimfiles_category = ListViewCategory('Vim files', 'dialog-information-symbolic')
+
+    def get_icon(self, uri):
+        """Returns a path to an icon for the given URI."""
+        g_file = Gio.file_new_for_uri(uri)
+        try:
+            icon = g_file.query_info(
+                    Gio.FILE_ATTRIBUTE_THUMBNAIL_PATH,
+                    Gio.FileQueryInfoFlags.NONE,
+                    None)
+        except _glib.GError: # eg file vanished
+            return self.vim_icon
+        icon_path = icon.get_attribute_as_string(
+                Gio.FILE_ATTRIBUTE_THUMBNAIL_PATH) or self.vim_icon
+        return icon_path
+
+    def handle_uri(self, scope, uri):
+        """Open the selected file with gvim."""
+        #print 'Handling %s' % uri
+        subprocess.Popen(['/usr/bin/gvim', uri])
+        return self.hide_dash_response(uri)
+
+    def match(self, search, path):
+        """Return true if the path matches the search pattern."""
+        # Use fnmatch for pattern matching (re.search is problematic since
+        # since the user might not enter a valid regular expression eg ~/.???).
+        # Use os.path.expanduser since vim can write unglobbed paths like
+        # ~user/foo to viminfo.
+        pattern = self.pattern(search)
+        #print 'Pattern %s' % pattern
+        #return re.search(search, expanduser(path)) or re.search(search, path)
+        return fnmatch(path, pattern) or fnmatch(expanduser(path), pattern)
+
+    def pattern(self, search):
+        """Returns a pattern suitable for fnmatch() from search string."""
+        # Anchor characters (^, $) are special here in the normal RE way.
+        # A leading tilde (~) acts to indicate a user home directory (as most shells).
+        # Quote a leading caret or tilde, or trailing backslash if you should want
+        # to search for a filename containing one of these characters.
+        # Leading special characters:
+        if re.search('^~', search):
+            pattern = expanduser(search) # expand leading tilde
+        elif re.search('^\\\~', search):
+            pattern = '*' + search[1:] # strip leading backslash before tilde
+        elif re.search('^\^', search):
+            pattern = search[1:] # strip leading caret
+        elif re.search('^\\\^', search):
+            pattern = '*' + search[1:] # strip leading backslash before caret
+        else:
+            pattern = '*' + search
+        # Trailing special characters:
+        if re.search('\$$', pattern):
+            pattern = pattern[:-1] # strip trailing dollar
+        elif re.search('^\\\$', pattern):
+            pattern = '*' + pattern[:-2] + '$' # strip backslash before trailing dollar
+        else:
+            pattern = pattern + '*'
+        return pattern
 
     def search(self, search, results):
-        # TODO: Add your search results
-        results.append('https://wiki.ubuntu.com/Unity/Lenses/Singlet',
-                         'ubuntu-logo',
-                         self.example_category,
-                         "text/html",
-                         'Learn More',
-                         'Find out how to write your Unity Lens',
-                         'https://wiki.ubuntu.com/Unity/Lenses/Singlet')
-        pass
+        """Perform the search and append to the results list."""
+        #print "Searching %s for %s" % (viminfo, search)
+        for path in self.viminfo_query(self.viminfo, search):
+            uri = 'file://%s' % expanduser(path)
+            results.append(uri,
+                    self.get_icon(uri), # icon path
+                    self.vimfiles_category, # category
+                    'text/plain', # MIME type
+                    path, # text
+                    '', # comment
+                    uri) # for drag and drop
+
+    def viminfo_query(self, viminfo, search):
+        """Return all matching file paths from the given viminfo file."""
+        # NB file might not exist any more, this is a feature.
+        return [f for f in self.viminfo_files(viminfo) if
+                self.match(search, f)]
+
+    def viminfo_files(self, viminfo):
+        """Return all file paths from the given viminfo file."""
+        # Pass in the viminfo path to this method for easier testing.
+        with open(viminfo) as v:
+            return [re.sub('^> ', '', f).rstrip('\n') for f in v.readlines() if re.match('> ', f)]
